57611acbcf169fdea1ae9c0fadc8e0913a9b2895
Ignacio Della Morte <ignaciodm93@gmail.com>
2020-12-02 09:24:45 -0300

    Actualizado 2.12.20

---
+++ src/main/java/com/examplecodeoftheweb/salvo_n/dto/ScoreDTO.java
@@ -0,0 +1,22 @@
+package com.examplecodeoftheweb.salvo_n.dto;
+
+import com.examplecodeoftheweb.salvo_n.model.Score;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ScoreDTO {
+
+
+    public static Map<String, Object> makeScoreDTO(Score s) {
+        Map<String, Object> dto = new LinkedHashMap<>();
+
+        dto.put("player", s.getPlayer().getId());
+        dto.put("score", s.getScore());
+        dto.put("finishDate", s.getFinishDate().getTime());
+
+        return dto;
+    }
+
+
+}
---
+++ src/main/java/com/examplecodeoftheweb/salvo_n/model/Score.java
@@ -0,0 +1,76 @@
+package com.examplecodeoftheweb.salvo_n.model;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private double score;
+    private Date finishDate;
+
+    //region Relaciones
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "player_id")
+    private Player player;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "game_id")
+    private Game game;
+    //endregion
+
+    //region Constructores
+    public Score(){
+
+    }
+
+    public Score(double score, Date finishDate, Player player, Game game) {
+        this.score = score;
+        this.finishDate = finishDate;
+        this.player = player;
+        this.game = game;
+    }
+    //endregion
+
+    //region Getters & Setters
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+    //endregion
+
+}
